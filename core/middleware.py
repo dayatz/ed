a0b11cf5763e1efea6267f5e5b31aaa51b7366a8
dayatz <dayat.py@gmail.com>
2015-04-08 11:03:28 +0800

    add materializecss, remove flat-ui

--- core/middleware.py
+++ core/middleware.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseRedirect
 class IsLoggedIn(object):
     def process_request(self, request):
         if not request.user.is_authenticated()\
-                and not request.path == reverse('home') and not request.path == reverse('login'):
+                and not request.path == reverse('home')\
+                and not request.path == reverse('login'):
             print 'u r not authenticated'
             return HttpResponseRedirect('/')
         pass
